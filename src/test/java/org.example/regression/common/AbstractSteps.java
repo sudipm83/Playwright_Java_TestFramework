179fa77a870a25fc0518e0d3791b5a82b837bc1b
sudipm83 <sudipm2000@gmail.com>
2025-05-03 20:46:41 +0530

    Accepting test url

--- src/test/java/org.example/regression/common/AbstractSteps.java
+++ src/test/java/org.example/regression/common/AbstractSteps.java
@@ -1,4 +1,11 @@
 package org.example.regression.common;
 
+import org.example.regression.common.enums.TestContext;
+
 public class AbstractSteps {
+    private final String defaultUrl = "https://www.saucedemo.com/";
+    private final String appTestUrl = System.getProperty("applicationTestUrl", defaultUrl);
+
+    public String getAppTestUrl() { return appTestUrl; }
+    public TestContext testContext() { return TestContext.CONTEXT; }
 }
